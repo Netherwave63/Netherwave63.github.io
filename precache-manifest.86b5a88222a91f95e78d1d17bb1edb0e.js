a12d31940d88ee9ad7b259ccc9723b07cdfb978e
Ethan Chew <YZ.chew@doo.hk>
2020-02-24 10:13:29 +0800

    Updates

---
+++ precache-manifest.86b5a88222a91f95e78d1d17bb1edb0e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0aaa3825c31e81c030179e9d7e9099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecb00a2d4a2c23d68e81",
+    "url": "/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "ecb00a2d4a2c23d68e81",
+    "url": "/static/js/2.45c1488c.chunk.js"
+  },
+  {
+    "revision": "2ae54b88a9a15ef01465",
+    "url": "/static/js/main.140441e5.chunk.js"
+  },
+  {
+    "revision": "c66bd39318662d7ab0bb",
+    "url": "/static/js/runtime-main.2940ff0c.js"
+  },
+  {
+    "revision": "cd0a2af07e785558dbbbd6a985171a01",
+    "url": "/static/media/hacker-news-1.cd0a2af0.png"
+  },
+  {
+    "revision": "e7475fe8d487e3d15307aa684fa170e5",
+    "url": "/static/media/hacker-news-2.e7475fe8.png"
+  },
+  {
+    "revision": "2a19314f30b107cb3aebc08dab92672f",
+    "url": "/static/media/jotter-auth-1.2a19314f.png"
+  },
+  {
+    "revision": "3ad321a407ea4e63d0a71c770d25d19c",
+    "url": "/static/media/jotter-auth-2.3ad321a4.png"
+  },
+  {
+    "revision": "9c929fa891eba5d8a620ab2d686fd6f4",
+    "url": "/static/media/jotter-auth-3.9c929fa8.png"
+  },
+  {
+    "revision": "bf44c758f889ad42ad754048a6fdf655",
+    "url": "/static/media/jotter-todos-1.bf44c758.png"
+  },
+  {
+    "revision": "bca9f042aaacf21208181853180c882a",
+    "url": "/static/media/jotter-todos-2.bca9f042.png"
+  }
+]);
